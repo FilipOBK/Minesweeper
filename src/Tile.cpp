e54653c7c575e49419c1493436ed2c5f4c2c935c
Filip Keerberg <filip.keerberg@gmail.com>
2024-07-12 18:03:16 +0200

    Moved implementation into cpp file, added some utility methods

---
+++ src/Tile.cpp
@@ -0,0 +1,41 @@
+#include "Tile.h"
+
+std::string Tile::GetChar()
+{
+    if (flagged) return "\033[1;33mF\033[0m";
+    if (covered) return " ";
+    if (bomb) return "\033[1;31mB\033[0m";
+    switch (surrounding)
+    {
+        case 0:
+            return "0";
+        case 1:
+            return "\033[1;94m1\033[0m";
+        case 2:
+            return "\033[1;32m2\033[0m";
+        case 3:
+            return "\033[1;31m3\033[0m";
+        case 4:
+            return "\033[1;35m4\033[0m";
+        case 5:
+            return "\033[1;33m5\033[0m";
+        case 6:
+            return "\033[1;33m6\033[0m";
+        case 7:
+            return "\033[1;33m7\033[0m";
+        case 8:
+            return "\033[1;33m8\033[0m";
+        default:
+            return "!";
+    }
+}
+
+void Tile::flag()
+{
+    if (covered) flagged = !flagged;
+}
+
+void Tile::uncover()
+{
+    covered = false;
+}
--- src/headers/Tile.h
+++ src/headers/Tile.h
@@ -4,38 +4,14 @@
 
 struct Tile
 {
+	int index;
 	int surrounding;
+
 	bool bomb;
 	bool flagged;
 	bool covered;
 
-	std::string GetChar()
-	{
-        if (flagged) return "\033[1;33mF\033[0m";
-        if (covered) return " ";
-        if (bomb) return "\033[1;31mB\033[0m";
-        switch (surrounding)
-        {
-            case 0:
-                return "0";
-            case 1:
-                return "\033[1;94m1\033[0m";
-            case 2:
-                return "\033[1;32m2\033[0m";
-            case 3:
-                return "\033[1;31m3\033[0m";
-            case 4:
-                return "\033[1;35m4\033[0m";
-            case 5:
-                return "\033[1;33m5\033[0m";
-            case 6:
-                return "\033[1;33m6\033[0m";
-            case 7:
-                return "\033[1;33m7\033[0m";
-            case 8:
-                return "\033[1;33m8\033[0m";
-            default:
-                return "!";
-        }
-	}
+	std::string GetChar();
+	void flag();
+	void uncover();
 };
